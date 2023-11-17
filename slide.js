67f9716267ee03e2a7fc09c306c9f052ecd6c39f
Seraphic00aa <mcancino_20ur1190@psu.edu.ph>
2023-11-18 01:37:42 +0800

    landingpage

--- slide.js
+++
@@ -1,9 +0,0 @@
-//slide
-const slider = document.querySelector('.slider');
-
-const leftArrow = document.querySelector('.left');
-const rightArrow = document.querySelector('.right');
-
-rightArrow.addEventListener('click', function(){
-	slider.style.transform = 'translate(-25%)';
-})
